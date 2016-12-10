12376e784317c1d4d51008be087b6b0224ced4f0
Jackson Smith <rjsmithdev@icloud.com>
2016-12-10 16:21:06 -0500

    Updated to custom fork of Pathkit

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "can-hack-honda",
     dependencies: [
-        .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 6)
+        .Package(url: "https://github.com/kylef/Commander", majorVersion: 0),
+		.Package(url: "https://github.com/rjsmith1999/PathKit", majorVersion: 0)
     ]
 )
