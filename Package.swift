caad6df9643ba0e818d8f5be92b5926057bcf28b
Jackson Smith <rjsmithdev@icloud.com>
2016-12-09 22:43:31 -0500

    Added dependency for Commander

--- Package.swift
+++ Package.swift
@@ -2,5 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "can-hack-honda",
-    
+    dependencies: [
+        .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 6)
+    ]
 )
