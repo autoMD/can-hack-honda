875d1dcb6299fe0b1a02f9abd4c5367ef42ca63a
Jackson Smith <rjsmithdev@icloud.com>
2016-12-10 21:22:22 -0500

    Added CAN data structures

---
+++ Sources/CAN.swift
@@ -0,0 +1,67 @@
+//
+//  CAN.swift
+//  can-hack-honda
+//
+//  Created by Jackson Smith on 12/10/16.
+//
+//
+
+import Foundation
+
+typealias Id = (UInt8, UInt8, UInt8)
+
+struct Frame {
+    var id: Id
+    var data: [UInt8]
+    
+    var length: Int {
+        return data.count
+    }
+    
+    init(_ id: Id, data: [UInt8]) {
+        self.id = id
+        self.data = data
+    }
+}
+
+func parse(hex: String) throws -> UInt8 {
+    guard let int = UInt8(hex, radix: 16) else { // convert hex string to UInt8
+        throw HondaCanFrame.ParseError.invalidHex
+    }
+    return int
+}
+
+class HondaCanFrame {
+    var frame: Frame
+    init(withFrame frame: Frame) {
+        self.frame = frame
+    }
+    
+    enum ParseError: Error {
+        case idNotFound
+        case dataNotFound
+        case invalidHex
+    }
+    
+    init(parse string: String) throws {
+        
+        guard let idRange = string.range(of: "0x([A-F0-9]{7,8})", options: .regularExpression) else { throw ParseError.idNotFound }
+        guard let dataRange = string.range(of: "(,[A-F0-9]{2})+$", options: .regularExpression) else { throw ParseError.dataNotFound }
+        
+        let idString = string.substring(with: idRange)
+        
+        let index3 = idString.index(idString.endIndex, offsetBy: -2)
+        let index2 = idString.index(idString.endIndex, offsetBy: -4)
+        let index1  = idString.index(idString.startIndex, offsetBy: 2)
+        
+        let ids = (
+            try parse(hex: idString.substring(with: index1..<index2)),
+            try parse(hex: idString.substring(with: index2..<index3)),
+            try parse(hex: idString.substring(from: index3))
+        )
+        
+        let data = try string.substring(with: dataRange).components(separatedBy: ",").map(parse(hex:))
+
+        self.frame = Frame(ids, data: data)
+    }
+}
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,7 +1,35 @@
 import Commander
+import PathKit
 
-let main = command {
-    print("Hello World")
+func parse(linesIn file: Path) {
+    do {
+        try file.read().enumerateLines { (line, bool) in
+            let frame = try? HondaCanFrame(parse: line)
+            
+            if let frame = frame {
+                print(frame)
+            } else {
+                print("Couldn't parse frame from line: \(line)")
+            }
+        }
+    } catch {
+        print("Error reading file: \(file)")
+    }
+}
+
+let main = command { (files: String) in
+    let path = Path(files)
+    
+    if path.isDirectory {
+        print("\(path) is a directory")
+        print("Iterating over all files")
+        
+        for file in path.glob("*") {
+            parse(linesIn: file)
+        }
+    } else if path.isFile {
+        parse(linesIn: path)
+    }
 }
 
 main.run()
