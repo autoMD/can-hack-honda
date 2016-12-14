2c0e8fe843badfba12223b17ef279444549a0577
Jackson Smith <rjsmithdev@icloud.com>
2016-12-13 19:18:15 -0500

    Update Project to begin to use new Libraries

--- Sources/Hack/main.swift
+++ Sources/Hack/main.swift
@@ -1,6 +1,23 @@
 import Commander
 import PathKit
 
+import Can
+
+// MARK: Functions
+
+/// Function to turn a string into a path, and print an error message if the string does not reference a flat file
+func coerceFile(_ pathString: String) -> Path? {
+    let path = Path(pathString)
+    
+    guard path.isFile else {
+        print("Error: \(path) is \(path.isDirectory ? "a directory, ":"") not a file")
+        
+        return nil
+    }
+    
+    return path
+}
+
 //func parse(linesIn file: Path, filter id: HondaCanID?) {
 //    do {
 //        try file.read().enumerateLines { (line, bool) in
@@ -21,10 +38,13 @@ import PathKit
 //    }
 //}
 //
-//command(
+
+// MARK: - Main
+
+command(
 //    Option("id", "", description: "Id to search for"),
-//    VariadicArgument("files", description: "Files to process")
-//) { (id: String, files: [String]) in
+    VariadicArgument("files", description: "Files to process")
+) { (inputFiles: [String]) in
 //    // Parse filter strings into HondaCanID so they can be compared later
 //    var searchId: HondaCanID? = nil
 //
@@ -35,19 +55,15 @@ import PathKit
 //            print("Error: couldn't parse filter id: \(id)")
 //        }
 //    }
-//
-//    for file in files {
-//        let path = Path(file)
-//
-//        guard path.isFile else {
-//            if path.isDirectory {
-//                print("Error: \(path) is a directory, not a file")
-//            } else {
-//                print("Error: \(path) is not a file")
-//            }
-//            break
-//        }
-//
-//        parse(linesIn: path, filter: searchId)
-//    }
-//}.run()
+
+    for inputFile in inputFiles {
+        guard let path = coerceFile(inputFile) else { break } // give up if not a file
+        
+        guard let fileContents: String = try? path.read() else { print("Error reading \(path)"); break }
+    
+        let dataSet = DataSet(parse: fileContents)
+        
+        print(dataSet)
+    }
+    
+}.run()
